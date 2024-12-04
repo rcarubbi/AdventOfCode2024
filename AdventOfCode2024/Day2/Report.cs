fc52d4d1de02a1b960fa9be6155d98388497ffd5
Raphael Carubbi <rcarubbi@gmail.com>
2024-12-04 11:05:47 +0000

    Add days 2, 3 and 4

---
+++ AdventOfCode2024/Day2/Report.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode2024.Day2;
+
+public class Report
+{
+    private readonly int[] _levels;
+
+    public Report(int[] levels)
+    {
+        _levels = levels;
+    }
+
+    public int[] Levels => _levels;
+
+
+    public bool IsAscendingSorted()
+    {
+        return _levels.SequenceEqual(_levels.Order());
+    }
+
+    public bool IsDescendingSorted()
+    {
+        return _levels.SequenceEqual(_levels.OrderDescending());
+    }
+
+    public bool AreAllGapsValid()
+    {
+        for (int i = 0; i < _levels.Length - 1; i++)
+        {
+            var gap = Math.Abs(_levels[i + 1] - _levels[i]);
+            if (gap > 3 || gap < 1)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsSafe()
+    {
+        return (IsAscendingSorted() || IsDescendingSorted()) && AreAllGapsValid();
+    }
+}
