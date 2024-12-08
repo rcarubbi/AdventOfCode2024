aa59c56cad80c8080a73b7e61145e69d93744bfd
Raphael Carubbi <rcarubbi@gmail.com>
2024-12-08 15:20:33 +0000

    add day 8 task 2

--- AdventOfCode2024/Day8/Solution.cs
+++ AdventOfCode2024/Day8/Solution.cs
@@ -7,7 +7,7 @@ public class Solution
 {
     private static string[] GetInput()
     {
-        var content = File.ReadAllLines("Day8\\Input.txt");
+        var content = File.ReadAllLines("Day8\\input.txt");
 
         return content;
     }
@@ -16,42 +16,140 @@ public class Solution
     public async Task Task1()
     {
         var map = GetInput();
-        var calculator = new AntinodeCalculator();
+        var calculator = new ClassicAntinodeCalculator();
         var repository = new MapRepository();
-        var counter = new AntinodeCounter(calculator, repository);
+        var counter = new AntinodeCounter(repository, calculator);
 
         int result = counter.CountUniqueAntinodes(map);
         result.Should().Be(289);
         Debug.WriteLine($"Day 8, Task 1 answer is: {result}");
     }
+
+    [Fact]
+    public async Task Task2()
+    {
+        var map = GetInput();
+        var repository = new MapRepository();
+        var calculator = new ResonantAntinodeCalculator(map[0].Length, map.Length);
+        var counter = new AntinodeCounter(repository, calculator);
+
+        int result = counter.CountUniqueAntinodes(map);
+        result.Should().Be(1030);
+        Debug.WriteLine($"Day 8, Task 2 answer is: {result}");
+    }
 }
+
 
 public record Antenna(char Frequency, (int X, int Y) Position);
 
 public interface IAntinodeCalculator
 {
-    IEnumerable<(int X, int Y)> CalculateAntinodes(Antenna a1, Antenna a2);
+    IEnumerable<(int X, int Y)> CalculateAntinodes(IEnumerable<Antenna> antennas);
 }
 
 
-public class AntinodeCalculator : IAntinodeCalculator
+public class ClassicAntinodeCalculator : IAntinodeCalculator
 {
-    public IEnumerable<(int X, int Y)> CalculateAntinodes(Antenna a1, Antenna a2)
+    public IEnumerable<(int X, int Y)> CalculateAntinodes(IEnumerable<Antenna> antennas)
     {
-        var dx = a2.Position.X - a1.Position.X;
-        var dy = a2.Position.Y - a1.Position.Y;
+        var antennaList = antennas.ToList();
+        var uniqueAntinodes = new HashSet<(int X, int Y)>();
 
-         
-        var antinode1 = (a1.Position.X - dx, a1.Position.Y - dy);
+        for (int i = 0; i < antennaList.Count; i++)
+        {
+            for (int j = i + 1; j < antennaList.Count; j++)
+            {
+                var a1 = antennaList[i];
+                var a2 = antennaList[j];
 
-      
-        var antinode2 = (a2.Position.X + dx, a2.Position.Y + dy);
+                var dx = a2.Position.X - a1.Position.X;
+                var dy = a2.Position.Y - a1.Position.Y;
 
-   
-        yield return antinode1;
-        yield return antinode2;
+                uniqueAntinodes.Add((a1.Position.X - dx, a1.Position.Y - dy));
+                uniqueAntinodes.Add((a2.Position.X + dx, a2.Position.Y + dy));
+            }
+        }
+
+        return uniqueAntinodes;
     }
 }
+public class ResonantAntinodeCalculator : IAntinodeCalculator
+{
+    private readonly int _mapWidth;
+    private readonly int _mapHeight;
+
+    public ResonantAntinodeCalculator(int mapWidth, int mapHeight)
+    {
+        _mapWidth = mapWidth;
+        _mapHeight = mapHeight;
+    }
+
+    public IEnumerable<(int X, int Y)> CalculateAntinodes(IEnumerable<Antenna> antennas)
+    {
+        var antennaList = antennas.ToList();
+        var uniqueAntinodes = new HashSet<(int X, int Y)>();
+
+        for (int i = 0; i < antennaList.Count; i++)
+        {
+            for (int j = i + 1; j < antennaList.Count; j++)
+            {
+                var a1 = antennaList[i];
+                var a2 = antennaList[j];
+
+                var dx = a2.Position.X - a1.Position.X;
+                var dy = a2.Position.Y - a1.Position.Y;
+
+                // Calcular o GCD para determinar os passos
+                var gcd = GCD(Math.Abs(dx), Math.Abs(dy));
+                var stepX = dx / gcd;
+                var stepY = dy / gcd;
+
+                // Estender para ambas as direções até os limites do mapa
+                ExtendLineToBounds(a1.Position, stepX, stepY, uniqueAntinodes);
+                ExtendLineToBounds(a2.Position, -stepX, -stepY, uniqueAntinodes);
+            }
+        }
+
+        // Adicionar as próprias antenas
+        foreach (var antenna in antennaList)
+        {
+            uniqueAntinodes.Add(antenna.Position);
+        }
+
+        return uniqueAntinodes;
+    }
+
+    private void ExtendLineToBounds((int X, int Y) start, int stepX, int stepY, HashSet<(int X, int Y)> antinodes)
+    {
+        int currentX = start.X;
+        int currentY = start.Y;
+
+        // Avançar para frente até os limites do mapa
+        while (IsWithinBounds(currentX, currentY))
+        {
+            antinodes.Add((currentX, currentY));
+            currentX += stepX;
+            currentY += stepY;
+        }
+    }
+
+    private bool IsWithinBounds(int x, int y)
+    {
+        return x >= 0 && x < _mapWidth && y >= 0 && y < _mapHeight;
+    }
+
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
 
 public interface IMapRepository
 {
@@ -60,11 +158,23 @@ public interface IMapRepository
 }
 public class MapRepository : IMapRepository
 {
+    private int _mapWidth;
+    private int _mapHeight;
+
+    public MapRepository()
+    {
+        _mapWidth = 0;
+        _mapHeight = 0;
+    }
+
     public IEnumerable<Antenna> GetAntennas(string[] map)
     {
-        for (int y = 0; y < map.Length; y++)
+        _mapHeight = map.Length;
+        _mapWidth = map[0].Length;
+
+        for (int y = 0; y < _mapHeight; y++)
         {
-            for (int x = 0; x < map[y].Length; x++)
+            for (int x = 0; x < _mapWidth; x++)
             {
                 char cell = map[y][x];
                 if (char.IsLetterOrDigit(cell))
@@ -75,18 +185,21 @@ public class MapRepository : IMapRepository
 
     public bool IsWithinBounds((int X, int Y) position, int width, int height)
     {
-        return position.X >= 0 && position.X < width &&
-               position.Y >= 0 && position.Y < height;
+        return position.X >= 0 && position.X < _mapWidth &&
+               position.Y >= 0 && position.Y < _mapHeight;
     }
+
+    public int GetMapWidth() => _mapWidth;
+    public int GetMapHeight() => _mapHeight;
 }
 public class AntinodeCounter
 {
-    private readonly IAntinodeCalculator _antinodeCalculator;
+    private readonly IAntinodeCalculator _antinodesCalculator;
     private readonly IMapRepository _mapRepository;
 
-    public AntinodeCounter(IAntinodeCalculator antinodeCalculator, IMapRepository mapRepository)
+    public AntinodeCounter(IMapRepository mapRepository, IAntinodeCalculator antinodesCalculator)
     {
-        _antinodeCalculator = antinodeCalculator;
+        _antinodesCalculator = antinodesCalculator;
         _mapRepository = mapRepository;
     }
 
@@ -97,21 +210,13 @@ public class AntinodeCounter
 
         foreach (var group in antennas)
         {
-            var antennaList = group.ToList();
+            var antinodes = _antinodesCalculator.CalculateAntinodes(group);
 
-            for (int i = 0; i < antennaList.Count; i++)
+            foreach (var antinode in antinodes)
             {
-                for (int j = i + 1; j < antennaList.Count; j++)
+                if (_mapRepository.IsWithinBounds(antinode, map.Length, map[0].Length))
                 {
-                    var antinodes = _antinodeCalculator.CalculateAntinodes(antennaList[i], antennaList[j]);
-
-                    foreach (var antinode in antinodes)
-                    {
-                        if (_mapRepository.IsWithinBounds(antinode, map[0].Length, map.Length))
-                        {
-                            uniqueAntinodes.Add(antinode);
-                        }
-                    }
+                    uniqueAntinodes.Add(antinode);
                 }
             }
         }
